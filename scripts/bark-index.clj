5c574ddb50ac33e649aac4ecc3d9d99905f0ac7f
Bastien Guerry <bzg@gnu.org>
2026-03-14 14:52:17 +0100

    fix: Don't set column width, just min-width

--- scripts/bark-index.clj
+++ scripts/bark-index.clj
@@ -186,7 +186,7 @@
   th[data-sort].desc::after { content: ' ↓'; opacity: 0.7; }
   tr.hidden { display: none; }
   tr.stripe td { background: rgba(115,130,140,.075); }
-  td:nth-child(4) { width: 45%; min-width: 300px; }
+  td:nth-child(4) { min-width: 300px; }
   td:nth-child(5) { max-width: 180px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; }
   #status { font-size: 0.8rem; margin-bottom: 0.5rem; }
   .vote-badge { display: inline-block; padding: 0.1rem 0.4rem; border-radius: 3px;
