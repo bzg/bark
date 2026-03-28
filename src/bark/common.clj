e642c6042ac28bf33632a623fc1213162f17aa02
Bastien Guerry <bzg@gnu.org>
2026-03-28 09:26:26 +0100

    feat: Bump bark-format to 0.2.3 to indicate recent additions

--- src/bark/common.clj
+++ src/bark/common.clj
@@ -21,7 +21,7 @@
 
 (def bark-format
   "BARK export format version. Bump when the JSON/Org export shape changes."
-  "0.2.2")
+  "0.2.3")
 
 (def bark-schema
   (edn/read-string (slurp (io/resource "bark-schema.edn"))))
