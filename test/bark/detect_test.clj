49db96711e6119fd8e04a682f357c6affed62911
Bastien Guerry <bzg@gnu.org>
2026-03-30 23:35:53 +0200

    fix: Improve attachment detection and export

--- test/bark/detect_test.clj
+++ test/bark/detect_test.clj
@@ -1,52 +1,53 @@
 (ns bark.detect-test
-  "Unit tests for bark.detect — ICS detection helpers."
+  "Unit tests for ICS detection helpers (common + detect)."
   (:require [clojure.test :refer [deftest is testing]]
+            [bark.common :as common]
             [bark.detect :as detect]))
 
 ;; ---------------------------------------------------------------------------
-;; ICS attachment detection
+;; ICS attachment detection (bark.common)
 ;; ---------------------------------------------------------------------------
 
 (deftest has-ics-attachment-test
   (testing "detects .ics attachment"
-    (is (detect/has-ics-attachment?
+    (is (common/has-ics-attachment?
          [{:attachment/filename "meeting.ics"}])))
 
   (testing "case-insensitive"
-    (is (detect/has-ics-attachment?
+    (is (common/has-ics-attachment?
          [{:attachment/filename "Event.ICS"}])))
 
   (testing "no .ics attachment"
-    (is (not (detect/has-ics-attachment?
+    (is (not (common/has-ics-attachment?
               [{:attachment/filename "readme.txt"}]))))
 
   (testing "nil attachments"
-    (is (not (detect/has-ics-attachment? nil))))
+    (is (not (common/has-ics-attachment? nil))))
 
   (testing "empty attachments"
-    (is (not (detect/has-ics-attachment? [])))))
+    (is (not (common/has-ics-attachment? [])))))
 
 ;; ---------------------------------------------------------------------------
-;; Inline ICS detection
+;; Inline ICS detection (bark.common)
 ;; ---------------------------------------------------------------------------
 
 (deftest has-inline-ics-test
   (testing "detects inline VCALENDAR with VEVENT"
-    (is (detect/has-inline-ics?
+    (is (common/has-inline-ics?
          "Some text\nBEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT\nSUMMARY:Test\nEND:VEVENT\nEND:VCALENDAR")))
 
   (testing "nil body"
-    (is (not (detect/has-inline-ics? nil))))
+    (is (not (common/has-inline-ics? nil))))
 
   (testing "VCALENDAR without VEVENT"
-    (is (not (detect/has-inline-ics?
+    (is (not (common/has-inline-ics?
               "BEGIN:VCALENDAR\nVERSION:2.0\nEND:VCALENDAR"))))
 
   (testing "plain text without ICS"
-    (is (not (detect/has-inline-ics? "Hello world")))))
+    (is (not (common/has-inline-ics? "Hello world")))))
 
 ;; ---------------------------------------------------------------------------
-;; Combined has-ics? check
+;; Combined has-ics? check (bark.detect)
 ;; ---------------------------------------------------------------------------
 
 (deftest has-ics-test
